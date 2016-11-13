81c1e3d4fa4d598220eb5541aed59f37e022387b
Philippe Schild <philippe.schild@googlemail.com>
2016-11-14 00:13:02 +0100

    - added mocha as test framework - added tests for testing merge algorithm

---
+++ test/test_B_k1_k2.js
@@ -0,0 +1,55 @@
+var assert = require('assert');
+
+var commit1 = require('./data/commit1_B_k1_k2.json');
+var commit2 = require('./data/commit2_B_k1_k2.json');
+
+import {CommitMerger} from '../js/CommitMerger';
+
+describe('CommitMerger', function () {
+    describe('#merge(commit1, commit2)', function () {
+        commit1.commitId = 'abc123';
+        commit2.commitId = 'def456';
+        var result = CommitMerger.merge(commit1, commit2);
+
+        it('should return an array', function () {
+            assert.ok(Array.isArray(result));
+        });
+
+        it('should contain the correct amount of elements', function () {
+            assert.equal(result.length, 1);
+            assert.equal(result[0].children.length, 2);
+        });
+
+        it('should contain correct metricValues for first class', function() {
+            var metricValues = result[0].children[0].metricValues;
+            assert.equal(typeof metricValues, 'object');
+
+            var metricNames = Object.keys(metricValues);
+            for (var metricName of metricNames) {
+                assert.equal(typeof metricValues[metricName], 'object');
+
+                var commitIds = Object.keys(metricValues[metricName]);
+                assert.ok(commitIds.indexOf('abc123') >= 0);
+                assert.ok(commitIds.indexOf('def456') < 0);
+
+                assert.equal(typeof metricValues[metricName]['abc123'], 'number');
+            }
+        });
+
+        it('should contain correct metricValues for second class', function() {
+            var metricValues = result[0].children[1].metricValues;
+            assert.equal(typeof metricValues, 'object');
+
+            var metricNames = Object.keys(metricValues);
+            for (var metricName of metricNames) {
+                assert.equal(typeof metricValues[metricName], 'object');
+
+                var commitIds = Object.keys(metricValues[metricName]);
+                assert.ok(commitIds.indexOf('abc123') < 0);
+                assert.ok(commitIds.indexOf('def456') >= 0);
+
+                assert.equal(typeof metricValues[metricName]['def456'], 'number');
+            }
+        });
+    });
+});
---
+++ test/test_CommitMerger.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+
+import {CommitMerger} from '../js/CommitMerger';
+
+describe('CommitMerger', function () {
+    describe('#_createMetricValueObjects(elements, commitId)', function () {
+        var json = {
+            type: 'ROOT',
+            children: [
+                {
+                    type: 'FILE',
+                    metricValues: {
+                        'metricName1': 111,
+                        'metricName2': 222
+                    },
+                    children: []
+                },
+                {
+                    type: 'MODULE',
+                    children: [
+                        {
+                            type: 'FILE',
+                            metricValues: {
+                                'metricName1': 333,
+                                'metricName2': 444
+                            },
+                            children: []
+                        }
+                    ]
+                }
+            ]
+        };
+
+        CommitMerger._createMetricValueObjects(json, 'abc123');
+
+        it('should create correct objects', function () {
+            var metricValuesOfFirstFile = json.children[0].metricValues;
+            var metricValuesOfSecondFile = json.children[1].children[0].metricValues;
+
+            assert.deepEqual(metricValuesOfFirstFile, {
+                'metricName1': {
+                    'abc123': 111
+                },
+                'metricName2': {
+                    'abc123': 222
+                }
+            });
+
+            assert.deepEqual(metricValuesOfSecondFile, {
+                'metricName1': {
+                    'abc123': 333
+                },
+                'metricName2': {
+                    'abc123': 444
+                }
+            });
+        });
+    });
+});
