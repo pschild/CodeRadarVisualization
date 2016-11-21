5d6e2754c510b5befbb68d397414effc70a5e90f
Philippe Schild <philippe.schild@googlemail.com>
2016-11-21 11:37:21 +0100

    using dependecies from script tags again instead of importing them. reason: keep build.js as small as possible.

--- js/Block.js
+++ js/Block.js
@@ -1,15 +1,13 @@
-import {Mesh, BoxGeometry, MeshLambertMaterial} from 'three';
-
-export class Block extends Mesh {
+export class Block extends THREE.Mesh {
     constructor(color, name) {
         var color = color || Math.random() * 0xffffff;
 
-        var geometry = new BoxGeometry(1, 1, 1);
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
 
         // move local coordinate system to scale the block properly
         geometry.translate(0.5, 0.5, 0.5);
 
-        var material = new MeshLambertMaterial({color: color});
+        var material = new THREE.MeshLambertMaterial({color: color});
 
         super(geometry, material);
 
