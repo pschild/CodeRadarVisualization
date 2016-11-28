d2208e8a806a4e79b2ddc29d56b8f990ea07ce0b
Philippe Schild <philippe.schild@googlemail.com>
2016-11-28 22:28:47 +0100

    fix: show date and time with leading zeros

--- js/domain/Commit.js
+++ js/domain/Commit.js
@@ -39,15 +39,15 @@ export class Commit {
     getFormattedDatetime() {
         let date = new Date(this.timestamp);
         return [
-            date.getDate(),
+            date.getDate() < 10 ? '0' + date.getDate() : date.getDate(),
             '.',
-            date.getMonth() + 1,
+            date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1,
             '.',
             date.getFullYear(),
             ' ',
-            date.getHours(),
+            date.getHours() < 10 ? '0' + date.getHours() : date.getHours(),
             ':',
-            date.getMinutes()
+            date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
         ].join('');
     }
 
