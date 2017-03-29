391032f002a15bc020cbf488edcf8c0499186667
pschild <philippe.schild@googlemail.com>
2017-03-29 16:52:04 +0200

    reformatted code

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { VisualizationPage } from './app.po';
+import {VisualizationPage} from './app.po';
 
 describe('visualization App', () => {
-  let page: VisualizationPage;
+    let page: VisualizationPage;
 
-  beforeEach(() => {
-    page = new VisualizationPage();
-  });
+    beforeEach(() => {
+        page = new VisualizationPage();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('app works!');
+    });
 });
