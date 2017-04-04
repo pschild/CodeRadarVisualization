96bdf0bb205cbca7470b0d22cc53caba65255779
Philippe Schild <philippe.schild@googlemail.com>
2017-04-04 19:32:47 +0200

    expanded mocked data

--- src/app/mocks/mock-data.ts
+++ src/app/mocks/mock-data.ts
@@ -7,7 +7,10 @@ export class MockData implements InMemoryDbService {
             { name: 'b152859ca8d73f5c974c2264107fd0092af310d0', author: 'John Doe', timestamp: 1485813773000, analyzed: true },
             { name: '2beb1d1d720c1256cedfdf483331f65861079705', author: 'John Doe', timestamp: 1485726067000, analyzed: true },
             { name: 'cbba0662f48f139da4973cc610bd4caa6213ed08', author: 'John Doe', timestamp: 1485633721000, analyzed: true },
-            { name: '6ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd', author: 'John Doe', timestamp: 1485561434000, analyzed: true }
+            { name: '6ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd', author: 'John Doe', timestamp: 1485561434000, analyzed: true },
+            { name: '7ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd', author: 'John Doe', timestamp: 1485561434001, analyzed: true },
+            { name: '8ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd', author: 'John Doe', timestamp: 1485561434002, analyzed: true },
+            { name: '9ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd', author: 'John Doe', timestamp: 1485561434003, analyzed: true }
         ];
         return {commits};
     }
