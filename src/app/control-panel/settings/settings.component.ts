97c8e1875d1c261f11e998cc5407485c816a8a52
Philippe Schild <philippe.schild@googlemail.com>
2017-04-06 13:49:07 +0200

    styling of control panel with bootstrap

--- src/app/control-panel/settings/settings.component.ts
+++ src/app/control-panel/settings/settings.component.ts
@@ -1,8 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {SettingsState} from "./settings.reducers";
-import {Store} from "@ngrx/store";
-import {AppState} from "../../shared/reducers";
-import {Subscription} from "rxjs/Subscription";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
     selector: 'app-settings',
@@ -11,22 +7,10 @@ import {Subscription} from "rxjs/Subscription";
 })
 export class SettingsComponent implements OnInit {
 
-    settingsState: SettingsState;
-
-    subscription: Subscription;
-
-    constructor(private store: Store<AppState>) {
+    constructor() {
     }
 
     ngOnInit() {
-        this.subscription = this.store.select(state => state.settingsState)
-            .subscribe((settingsState) => {
-                this.settingsState = settingsState;
-            });
-    }
-
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
     }
 
 }
