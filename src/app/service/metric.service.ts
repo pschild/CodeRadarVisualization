7e8754b46817a6c72cbdb57b1349d76ffc0dc6f8
pschild <philippe.schild@googlemail.com>
2017-04-27 23:03:03 +0200

    added more dummy data to simulate commit change

--- src/app/service/metric.service.ts
+++ src/app/service/metric.service.ts
@@ -18,8 +18,24 @@ export class MetricService {
             'metrics': [metricMapping.heightMetricName, metricMapping.groundAreaMetricName, metricMapping.colorMetricName]
         };
 
+        let id;
+        switch (rightCommit.name) {
+            case 'b152859ca8d73f5c974c2264107fd0092af310d0':
+                id = 1;
+                break;
+            case '2beb1d1d720c1256cedfdf483331f65861079705':
+                id = 2;
+                break;
+            case 'cbba0662f48f139da4973cc610bd4caa6213ed08':
+                id = 3;
+                break;
+            case '6ffebfad9e79dfa4ddfa7d043d84eb424a28c0cd':
+                id = 4;
+                break;
+        }
+
         // TODO: this.http.post('http://localhost:4200/assets/json/deltaTree.json', body)
-        return this.http.get('http://localhost:4200/assets/json/deltaTree.json')
+        return this.http.get(`http://localhost:4200/assets/json/deltaTree${id}.json`)
             .map((res) => {
                 return {
                     rootNode: res.json()
